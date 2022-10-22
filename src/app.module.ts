7b38a80d4800e60c262780fe51fb9af70f59eec6
Sebastian Ruiz <sebasruiz909@gmail.com>
2022-10-22 11:39:13 -0500

    ADD : docker compose for postgres db and TYPE ORM setup

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,5 +1,7 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { ServeStaticModule } from '@nestjs/serve-static';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { join } from 'path';
 
 @Module({
@@ -7,6 +9,21 @@ import { join } from 'path';
     ServeStaticModule.forRoot({
       rootPath: join(__dirname, '..', 'public'),
     }),
+    ConfigModule.forRoot(),
+    // module for typeorm use
+    TypeOrmModule.forRoot(
+      // object config
+      {
+        type: 'postgres',
+        host: process.env.DB_HOST,
+        port: +process.env.DB_PORT,
+        database: process.env.DB_NAME,
+        username: process.env.DB_USERNAME,
+        password: process.env.DB_PASSWORD,
+        autoLoadEntities: true,
+        synchronize: true,
+      },
+    ),
   ],
 })
 export class AppModule {}
